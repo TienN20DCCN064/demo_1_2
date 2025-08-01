df002db905151d934f863e8f41621f15748bdfb6
Lê Văn Tiến <vantien18122002@gmail.com>
2025-08-01 15:33:46 +0700

    tt

--- src/components/UserListItem.js
+++ src/components/UserListItem.js
@@ -14,56 +14,50 @@ const UserListItem = ({ user, onDeleteClick, onUpdateClick }) => {
     };
 
     return (
-        <div style={{ display: 'flex' }}>
-            <div style={{ paddingLeft: '10px' }}>
-                {user.id}
-            </div>
-            <div style={{
-                marginLeft: '100px',
-                textAlign: 'center',
-                height: '40px',
-                width: '40px',
-                lineHeight: '40px',
-                borderRadius: '50%',
-                color: 'white',
-                fontWeight: 'bold',
-                background: stringToHslColor(user.firstName + user.lastName)
-            }}>
-                {!!user && !!user.firstName && !!user.lastName ? user.firstName[0].toUpperCase() + user.lastName[0].toUpperCase() : ''}
-            </div>
-            <div style={{ margin: 'auto 0', flexGrow: 1, paddingLeft: '10px' }}>
-                {user.firstName} {user.lastName}
-            </div>
-            <div style={{ margin: 'auto 0' }}>
-                <Button size="sm" color="danger" outline onClick={() => {
-                    if (window.confirm("bạn có muốn t xóa ko")) {
-                        onDeleteClick(user.id);
-                    }
-                }}
-                >
-                    Delete
-                </Button>
-
-
-                <Button size="sm" color="blue" outline onClick={() => {
-                    if (window.confirm(`bạn có muốn sua ko ${user.id}`)) {
-                        onUpdateClick({
-                            userId: user.id,
-                            firstName: document.getElementById("firstName").value,
-                            lastName: document.getElementById("lastName").value
-                        });
-                    }
-                }}
-                >
-                    Update
-                </Button>
-
-
-
-
-            </div>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ paddingLeft: '10px', width: '50px', textAlign: 'left' }}>
+            {user.id}
         </div>
-    );
+        <div style={{
+            marginLeft: '100px',
+            textAlign: 'center',
+            height: '40px',
+            width: '40px',
+            lineHeight: '40px',
+            borderRadius: '50%',
+            color: 'white',
+            fontWeight: 'bold',
+            background: stringToHslColor(user.firstName + user.lastName)
+        }}>
+            {!!user && !!user.firstName && !!user.lastName ? user.firstName[0].toUpperCase() + user.lastName[0].toUpperCase() : ''}
+        </div>
+        <div style={{ margin: 'auto 0', flexGrow: 1, paddingLeft: '10px' }}>
+            {user.firstName} {user.lastName}
+        </div>
+        <div style={{ margin: 'auto 0' }}>
+            <Button size="sm" color="danger" outline onClick={() => {
+                if (window.confirm("bạn có muốn t xóa ko")) {
+                    onDeleteClick(user.id);
+                }
+            }}
+            >
+                Delete
+            </Button>
+            <Button size="sm" color="blue" outline onClick={() => {
+                if (window.confirm(`bạn có muốn sua ko ${user.id}`)) {
+                    onUpdateClick({
+                        userId: user.id,
+                        firstName: document.getElementById("firstName").value,
+                        lastName: document.getElementById("lastName").value
+                    });
+                }
+            }}
+            >
+                Update
+            </Button>
+        </div>
+    </div>
+);
 };
 
 export default UserListItem;
